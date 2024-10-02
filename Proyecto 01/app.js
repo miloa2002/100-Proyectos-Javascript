f0c8f21bffc074252ca138b878d57f91c1f06c26
miloa2002 <agudelo.tapias@gmail.com>
2024-10-02 08:24:00 -0500

    FEAT: Proyecto 1 lógica

---
+++ Proyecto 01/app.js
@@ -0,0 +1,22 @@
+
+const nombre = "Camilo";
+const apellido = "Agudelo";
+let edad = 22;
+const pais = "Colombia";
+let conocimientos = ["desarrollo web", "diseño gráfico"]
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+console.log(pais);
+console.log(conocimientos);
+
+
+edad = 24;
+conocimientos.push("Producción audiovisual");
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+console.log(pais);
+console.log(conocimientos);
