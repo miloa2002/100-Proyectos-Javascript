d8287f7e24f26f9d079c9031ac159566906d37e2
miloa2002 <agudelo.tapias@gmail.com>
2024-10-02 09:14:45 -0500

    FEAT: Ejercicio 3 lógica

---
+++ Proyecto 03/app.js
@@ -0,0 +1,12 @@
+
+let coche;
+const MAX = 135;
+const LIMITE = 60;
+
+const VELOCIDAD = Math.floor(Math.random() * 201);
+
+if(VELOCIDAD > LIMITE) {
+    alert("Vas muy rápido" + "" + `${VELOCIDAD}/km`)
+}else {
+    alert("Vas a una velocidad prudente" + "" + `${VELOCIDAD}/km`)
+}
